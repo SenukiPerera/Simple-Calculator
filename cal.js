71a0d801d25de3acd0d86e2cfc24a2d5aeaba7ec
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2024-02-01 16:01:40 +0530

    Simple Calculator

---
+++ cal.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteValue(){
+    display.value = display.value.toString().slice(0,-1);
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
